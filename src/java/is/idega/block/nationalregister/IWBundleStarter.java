8fe33bd953e40cbb04103c96964019b3731abbb5
Thomas Hilbig <thomas@idega.com>
2004-08-27 15:31:41 +0000

    implementation of plugin for Toolbar

--- src/java/is/idega/block/nationalregister/IWBundleStarter.java
+++ src/java/is/idega/block/nationalregister/IWBundleStarter.java
@@ -1,12 +1,10 @@
 package is.idega.block.nationalregister;
 
-import is.idega.block.nationalregister.business.NationalRegisterFileImportHandler;
-import is.idega.block.nationalregister.data.NationalRegisterImportFile;
+import is.idega.block.nationalregister.business.NationalRegisterFileImportHandlerPlugin;
 import com.idega.idegaweb.IWBundle;
 import com.idega.idegaweb.IWBundleStartable;
 import com.idega.repository.data.ImplementorRepository;
-import com.idega.user.handler.UserNationalRegisterFileImportHandler;
-import com.idega.user.handler.UserNationalRegisterImportFile;
+import com.idega.user.app.ToolbarElement;
 
 /**
  * <p>Title: idegaWeb</p>
@@ -22,8 +20,7 @@ public class IWBundleStarter implements IWBundleStartable {
 	public void start(IWBundle starterBundle) {
 		// add implementors for the com.idega.user bundle
 		ImplementorRepository repository =  ImplementorRepository.getInstance();
-		repository.addImplementor(UserNationalRegisterImportFile.class,NationalRegisterImportFile.class);
-		repository.addImplementor(UserNationalRegisterFileImportHandler.class, NationalRegisterFileImportHandler.class);
+		repository.addImplementor(ToolbarElement.class,NationalRegisterFileImportHandlerPlugin.class);
 	}
 	
 	public void stop(IWBundle starterBundle) {
---
+++ src/java/is/idega/block/nationalregister/business/NationalRegisterFileImportHandlerPlugin.java
@@ -0,0 +1,76 @@
+package is.idega.block.nationalregister.business;
+
+import is.idega.block.nationalregister.data.NationalRegisterImportFile;
+import java.util.HashMap;
+import java.util.Map;
+import com.idega.block.importer.presentation.Importer;
+import com.idega.idegaweb.IWBundle;
+import com.idega.idegaweb.IWMainApplicationSettings;
+import com.idega.idegaweb.IWResourceBundle;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.Image;
+import com.idega.user.app.ToolbarElement;
+
+
+/**
+ * <p>Title: idegaWeb</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Company: idega Software</p>
+ * @author <a href="thomas@idega.is">Thomas Hilbig</a>
+ * @version 1.0
+ * Created on Aug 27, 2004
+ */
+public class NationalRegisterFileImportHandlerPlugin implements ToolbarElement {
+	
+	private final static String IW_BUNDLE_IDENTIFIER = "is.idega.block.nationalregister";
+
+	/* (non-Javadoc)
+	 * @see com.idega.user.app.ToolbarElement#getButtonImage(com.idega.presentation.IWContext)
+	 */
+	public Image getButtonImage(IWContext iwc) {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.user.app.ToolbarElement#getName(com.idega.presentation.IWContext)
+	 */
+	public String getName(IWContext iwc) {
+		IWBundle bundle = iwc.getApplicationContext().getIWMainApplication().getBundle(IW_BUNDLE_IDENTIFIER);
+		IWResourceBundle resourceBundle = bundle.getResourceBundle(iwc);
+		return resourceBundle.getLocalizedString("button.national_register", "National Register");
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.user.app.ToolbarElement#getPresentationObjectClass(com.idega.presentation.IWContext)
+	 */
+	public Class getPresentationObjectClass(IWContext iwc) {
+		return Importer.class;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.user.app.ToolbarElement#getParameterMap(com.idega.presentation.IWContext)
+	 */
+	public Map getParameterMap(IWContext iwc) {
+		Map map = new HashMap();
+		map.put(Importer.PARAMETER_IMPORT_FILE,  NationalRegisterImportFile.class.getName());
+		map.put(Importer.PARAMETER_IMPORT_HANDLER, NationalRegisterFileImportHandler.class.getName());
+		return map;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.user.app.ToolbarElement#isValid(com.idega.presentation.IWContext)
+	 */
+	public boolean isValid(IWContext iwc) {
+		IWMainApplicationSettings settings = iwc.getApplicationSettings();
+		return (iwc.isSuperAdmin() && settings.getProperty("temp_show_is_related_stuff") != null);
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.user.app.ToolbarElement#getPriority(com.idega.presentation.IWContext)
+	 */
+	public int getPriority(IWContext iwc) {
+		return 8;
+	}
+}
