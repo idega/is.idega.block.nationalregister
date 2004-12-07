85ea4eef29da19ff1fc49c74a17b29686b270f7b
Eirikur Hrafnsson <eiki@idega.com>
2004-12-07 18:04:46 +0000

    a little more refactoring of UserGroupPluginBusiness

--- src/java/is/idega/block/nationalregister/business/NationalRegisterBusinessBean.java
+++ src/java/is/idega/block/nationalregister/business/NationalRegisterBusinessBean.java
@@ -426,9 +426,9 @@ public class NationalRegisterBusinessBean extends IBOServiceBean implements Nati
 	}
 
 	/* (non-Javadoc)
-	 * @see com.idega.user.business.UserGroupPlugInBusiness#canCreateSubGroup(com.idega.user.data.Group)
+	 * @see com.idega.user.business.UserGroupPlugInBusiness#canCreateSubGroup(com.idega.user.data.Group,java.lang.String)
 	 */
-	public String canCreateSubGroup(Group group) throws RemoteException {
+	public String canCreateSubGroup(Group group, String groupTypeOfSubGroup) throws RemoteException {
 		// TODO Auto-generated method stub
 		return null;
 	}
