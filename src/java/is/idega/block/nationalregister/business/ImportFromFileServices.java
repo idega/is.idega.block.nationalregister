1fa6c763dc7513aa51758bff5b87c1d2bdce141a
Valdas Žemaitis <valdas@idega.com>
2016-05-18 15:45:19 +0300

    New method to import

--- src/java/is/idega/block/nationalregister/business/ImportFromFileServices.java
+++ src/java/is/idega/block/nationalregister/business/ImportFromFileServices.java
@@ -11,11 +11,15 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import com.idega.block.importer.data.ImportFile;
 import com.idega.core.business.DefaultSpringBean;
 import com.idega.idegaweb.IWResourceBundle;
 import com.idega.presentation.IWContext;
 import com.idega.util.CoreUtil;
+import com.idega.util.StringUtil;
 
+import is.idega.block.nationalregister.data.NationalRegisterImportFile;
+import is.idega.block.nationalregister.data.NationalRegisterImportFileE32;
 import is.idega.block.nationalregister.data.NationalRegisterImportFileE36;
 
 @Service(ImportFromFileServices.BEAN_NAME)
@@ -34,24 +38,53 @@ public class ImportFromFileServices extends DefaultSpringBean {
 	}
 
 	@RemoteMethod
-	public String importFromFile(String filePath){
+	public String importFromFile(String filePath, String type) {
 		IWContext iwc = CoreUtil.getIWContext();
 		IWResourceBundle iwrb = getIwrb(iwc);
-		try{
+		try {
 			if (!iwc.isLoggedOn() || !iwc.getAccessController().isAdmin(iwc)) {
 				throw new Exception("Do not have permission");
 			}
-			NationalRegisterFileImportHandler handler = new NationalRegisterFileImportHandlerBean();
 
-			NationalRegisterImportFileE36 file = new NationalRegisterImportFileE36();
-			file.setFile(new File(filePath));
-			handler.setImportFile(file);
-			handler.handleRecords();
-			return iwrb.getLocalizedString("success", "Success");
-		}catch (Exception e) {
+			if (doImport(filePath, type)) {
+				return iwrb.getLocalizedString("success", "Success");
+			}
+		} catch (Exception e) {
 			getLogger().log(Level.WARNING, "Failed importing from file: " + filePath, e);
 		}
 		return iwrb.getLocalizedString("error", "Error");
+	}
+
+	public boolean doImport(String filePath, String type) {
+		ImportFile file = null;
+		try {
+			NationalRegisterFileImportHandler handler = new NationalRegisterFileImportHandlerBean();
+
+			type = StringUtil.isEmpty(type) ? "E36" : type;
+			switch (type) {
+				case "E36": {
+					file = new NationalRegisterImportFileE36();
+					break;
+				}
+				case "E32": {
+					file = new NationalRegisterImportFileE32();
+					break;
+				}
+				default: {
+					file = new NationalRegisterImportFile();
+				}
+			}
+
+			file.setFile(new File(filePath));
+			handler.setImportFile(file);
+			handler.handleRecords();
+
+			return true;
+		} catch (Exception e) {
+			getLogger().log(Level.WARNING, "Failed importing from file: " + filePath + ", importer: " + file.getClass().getName(), e);
+		}
+
+		return false;
 	}
 
 }
