f6aa97623a32c8d2973c6ef99bcdcea2656b497a
L4p1n45 <martynas.stake@gmail.com>
2018-01-23 18:45:14 +0200

    Update method

--- src/java/is/idega/block/nationalregister/data/NationalRegisterFateHome.java
+++ src/java/is/idega/block/nationalregister/data/NationalRegisterFateHome.java
@@ -6,9 +6,11 @@ package is.idega.block.nationalregister.data;
 
 import java.util.Collection;
 
+import javax.ejb.CreateException;
 import javax.ejb.FinderException;
 
 import com.idega.data.IDOHome;
+import com.idega.data.IDOStoreException;
 
 /**
  * @author bluebottle
@@ -31,4 +33,13 @@ public interface NationalRegisterFateHome extends IDOHome {
 	public NationalRegisterFate findByFateCode(String fateCode)
 			throws FinderException;
 
+	/**
+	 * 
+	 * @param fateCode which is recognized by system, not <code>null</code>
+	 * @param description to define this fate, skipped if <code>null</code>
+	 * @return updated/created record or <code>null</code> on failure
+	 * @throws CreateException 
+	 * @throws IDOStoreException 
+	 */
+	public NationalRegisterFate update(String fateCode, String description) throws IDOStoreException, CreateException;
 }
--- src/java/is/idega/block/nationalregister/data/NationalRegisterFateHomeImpl.java
+++ src/java/is/idega/block/nationalregister/data/NationalRegisterFateHomeImpl.java
@@ -5,10 +5,14 @@ package is.idega.block.nationalregister.data;
 
 
 import java.util.Collection;
+import java.util.logging.Level;
 
+import javax.ejb.CreateException;
 import javax.ejb.FinderException;
 
 import com.idega.data.IDOFactory;
+import com.idega.data.IDOStoreException;
+import com.idega.util.StringUtil;
 
 /**
  * @author bluebottle
@@ -46,4 +50,30 @@ public class NationalRegisterFateHomeImpl extends IDOFactory implements
 		return this.findByPrimaryKey(pk);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see is.idega.block.nationalregister.data.NationalRegisterFateHome#update(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public NationalRegisterFate update(String fateCode, String description) throws IDOStoreException, CreateException {
+		NationalRegisterFate entity = null;
+		try {
+			entity = findByFateCode(fateCode);
+		} catch (FinderException e) {
+			getLog().log(Level.INFO, "No entry found by argument: " + fateCode);
+		}
+
+		if (entity == null) {
+			entity = create();
+		}
+
+		entity.setFateCode(fateCode);
+		if (!StringUtil.isEmpty(description)) {
+			entity.setFateString(description);
+		}
+
+		entity.store();
+
+		return entity;
+	}
 }
